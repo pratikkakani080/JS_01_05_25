1ced3d16888ebaa03e33833ba626da03360657f6
Pratik <pratik080.rejoice@gmail.com>
2025-05-01 11:17:30 +0530

    Initial commit

---
+++ dataTypes.js
@@ -0,0 +1,53 @@
+// Premitive or Simple
+    // String
+        // '', "", ``
+        // const age = 29
+        // 'Pratik', "Pratik's", `Pratik's age is ${age}`
+    // Number
+        // 123423, 3.56 // 2**53
+    // Boolean
+        // true, false
+    // Null
+        // null
+    // Undefined
+        // undefined
+    // Bigint
+        // 9007199254740992n + 1n
+    // Symbol
+        // Symbol() // () - call sign
+
+// Complex
+    // Array
+        // ['JOhn', 'JOhn', 'Pratik'] - Array of string - should be singular
+        // [1, 3, 4 ,6 ,7 ,5 ,3 ,3] - Array od numbers
+        // [{
+        //     Name: 'Pratik',
+        //     age: 29,
+        //     Hobbies: ['Reading', 'Riding']
+        // }, {
+        //     Name: 'Pratik',
+        //     age: 29,
+        //     Hobbies: ['Reading', 'Riding']
+        // }, {
+        //     Name: 'Pratik',
+        //     age: 29,
+        //     Hobbies: ['Reading', 'Riding']
+        // }] - Array of objet 
+    // Object
+        // {
+        //     Name: 'Pratik',
+        //     age: 29,
+        //     Hobbies: ['Reading', 'Riding']
+        // }
+    // Function
+        // console.log('testing outside handler');
+        // // Normal function
+        // function testingFun() {
+        //     console.log('testing function call');
+        // }
+        // testingFun()
+        // // Arrow function
+        // const arrowFun = () => {
+        //     console.log('testing arrow function call');
+        // }
+        // arrowFun()
