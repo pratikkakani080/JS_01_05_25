c88a72870f665f68b6aea3b01ff8e87254073416
Pratik <pratik080.rejoice@gmail.com>
2025-05-29 11:07:12 +0530

    add: operators

---
+++ operators.js
@@ -0,0 +1,18 @@
+// Arithmatic operator
+// + - / * ** % ++ --
+
+// Assignment operator
+// = += -= *= **= %= /=
+
+// Comparision operator
+// == === != !== > < >= <= ?
+
+// ternary operator
+// let color = 'blue'
+// let fName = 'john'
+// color = fName === 'john'  ? color : (fName === 'tony' ? 'yellow' : (fName === 'sweety' ? 'black' : 'red')) // nested ternary
+// console.log('color', color);
+
+// Logical operator
+// && || !
+// 5 === '5' && 4 === 4
