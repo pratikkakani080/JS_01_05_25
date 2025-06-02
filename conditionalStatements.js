d1f82b06063b32cf426eefd62f4bee11cd2c04ab
Pratik <pratik080.rejoice@gmail.com>
2025-06-02 11:15:57 +0530

    add: conditional + loop

---
+++ conditionalStatements.js
@@ -0,0 +1,26 @@
+// if..else
+let color = 'red'
+if (color === 'red') {
+    console.log('red**');
+} else if (color === 'black') {
+    console.log('black**');
+} else {
+    console.log('default**');
+}
+
+// switch..case
+switch(color) {
+    case 'red':
+        console.log('color is red');
+        break;
+    
+    case 'black':
+        console.log('color is black');
+        break;
+    
+    default:
+        console.log('color is default');
+        break;
+}
+
+// ternary operator
---
+++ looping.js
@@ -0,0 +1,43 @@
+// for loop
+// const a = [2, 4, 6, 8, 6, 4, 2, 2, 3, 4, 6]
+// for (let i = 0; i < a.length; i++) {
+//     // a.push(i)
+//     console.log(i, a[i], 'inside loop');
+
+// }
+// console.log(a, 'outside loop');
+
+// while loop
+// let b = 0
+// while(b < 10) {
+//     console.log(b);
+
+//     b++
+// }
+// console.log(b);
+
+// do while loop
+// let c = 100
+// do {
+//     console.log(c);
+//     c--
+// } while (c > 80)
+// console.log(c);
+
+// for in loop - only loop that can iterate objects
+// const d = { test: 'testddd', test1: 'testddd1' }
+// for (const key in d) {
+//     console.log(key);
+//     console.log(d[key]);
+
+// }
+
+// for of loop
+// const e = [3, 5, 5, 6, 7,45,3423, 12, 6,43]
+// for (const element of e) {
+//     if (element.toString().length > 2) {
+//         continue;
+//     }
+//     console.log(element);
+    
+// }
